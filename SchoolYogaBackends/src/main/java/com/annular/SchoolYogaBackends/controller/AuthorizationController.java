041cbe7605de4c37582fd26ec177a0c11568ab08
benishaBM <benisha.b@annulartechnologies.com>
2025-02-25 17:58:19 +0530

    Added sso response

--- SchoolYogaBackends/src/main/java/com/annular/SchoolYogaBackends/controller/AuthorizationController.java
+++ SchoolYogaBackends/src/main/java/com/annular/SchoolYogaBackends/controller/AuthorizationController.java
@@ -13,10 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.annular.SchoolYogaBackends.Response;
+import com.annular.SchoolYogaBackends.model.AvatarImage;
 import com.annular.SchoolYogaBackends.model.RefreshToken;
+import com.annular.SchoolYogaBackends.model.SmileImage;
 import com.annular.SchoolYogaBackends.model.User;
+import com.annular.SchoolYogaBackends.repository.AvartarImageRepository;
+import com.annular.SchoolYogaBackends.repository.SmileImageRepository;
 import com.annular.SchoolYogaBackends.repository.UserRepository;
 import com.annular.SchoolYogaBackends.security.jwt.JwtGenerator;
+import com.annular.SchoolYogaBackends.security.jwt.JwtResponse;
 import com.annular.SchoolYogaBackends.service.AuthenticationService;
 import com.annular.SchoolYogaBackends.webModel.UserWebModel;
 
@@ -35,28 +40,64 @@ public class AuthorizationController {
 	AuthenticationService userService;
 	
 	@Autowired
+	AvartarImageRepository avatarImageRepository;
+	
+	@Autowired
+	SmileImageRepository smileImageRepository;
+
+	
+	@Autowired
 	JwtGenerator jwtGenerator;
 	
 	@PostMapping("/ssoCheck")
 	public ResponseEntity<?> ssoCheck(@RequestBody UserWebModel userWebModel) {
-		Optional<User> data = userRepository.findByEmail(userWebModel.getEmailId());
-		if (data.isPresent()) {
-			try {
-	            String idToken = userWebModel.getIdToken();
-//	            String jwt = googleAuthService.verifyAndGenerateJwt(idToken);
-	            RefreshToken refreshToken = userService.createRefreshToken(userWebModel);
+	    Optional<User> checkUser = userRepository.findByEmail(userWebModel.getEmailId());
+	    
+	    if (checkUser.isPresent()) {
+	        try {
+	            User user = checkUser.get();
+
+	            // Fetch profile picture URL if available
+	            String profilePicUrl = Optional.ofNullable(user.getProfilePic())
+	                .flatMap(avatarImageRepository::findById)
+	                .map(AvatarImage::getPath)
+	                .map(path -> "https://schoolyogabackend.s3.ap-south-1.amazonaws.com/" + path)
+	                .orElse(null);
+
+	            // Fetch smile picture URL if available
+	            String smilePicUrl = Optional.ofNullable(user.getSmilePic())
+	                .flatMap(smileImageRepository::findById)
+	                .map(SmileImage::getPath)
+	                .map(path -> "https://schoolyogabackend.s3.ap-south-1.amazonaws.com/" + path)
+	                .orElse(null);
+
+	            // Generate JWT token
 	            String jwt = jwtGenerator.generateJwt(userWebModel.getAccessToken());
-	            Map<String, Object> responseData = new HashMap<>();
-	            responseData.put("jwt", jwt);
-	            responseData.put("userId", data.get().getUserId());
-	            responseData.put("token", refreshToken);
-	            return ResponseEntity.ok(responseData);
+
+	            // Create refresh token
+	            RefreshToken refreshToken = userService.createRefreshToken(userWebModel);
+
+	            // Return response with JWT and refresh token
+	            return ResponseEntity.ok(new JwtResponse(
+	                jwt, 
+	                user.getUserId(), 
+	                1, // Assuming this represents a status or role value
+	                refreshToken.getToken(), 
+	                user.getUserType(), 
+	                user.getStd(), 
+	                user.getUserName(), 
+	                profilePicUrl, 
+	                smilePicUrl
+	            ));
+
 	        } catch (Exception e) {
 	            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
 	        }
-		}
-		return ResponseEntity.ok().body(new Response(-1, "User not found.Please register", ""));
+	    }
+	    
+	    return ResponseEntity.ok().body(new Response(-1, "User not found. Please register", ""));
 	}
+
 
 
 }
